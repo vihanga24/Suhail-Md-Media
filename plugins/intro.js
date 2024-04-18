e87c5437a4ce7edf9683aacca188072a4218712f
vihanga24 <85000887+vihanga24@users.noreply.github.com>
2024-04-18 12:36:01 +0530

    Update intro.js

--- plugins/intro.js
+++ plugins/intro.js
@@ -45,20 +45,19 @@ CURRENTLY RUNNING ON BETA VERSION!!
 const { smd, Config,smdBuffer,  prefix } = require('../lib')
 
 
-var surl = 'https://github.com/SuhailTechInfo/Secktor-bot' // Source URL
-const number = '923184474176'
-var name = ' Suhail Tech'
+var surl = 'https://instagram.com/mr.vi_x_0' // Source URL
+const number = '94752799279'
+var name = ' Vihanga.xx'
 var body = '𝑇𝛩𝑈𝐶𝛨 𝛨𝛯𝑅𝛯'
-var image = 'https://telegra.ph/file/1e60489705c851f74b55e.jpg'
+var image = 'https://i.imgur.com/9nEfkh0.jpg'
 let text = `╭═══ ━ ━ ━ ━ • ━ ━ ━ ━ ═══♡᭄
 │       「 𝗠𝗬 𝗜𝗡𝗧𝗥𝗢 」
-│ Name      : sυнαɪʟ ᴛᴇᴄʜ ɪиғօ
-│ Place       : ʟαнᴏʀᴇ ᴘᴋ
+│ Name      : ᴠɪʜᴀɴɢᴀ.xx💀☢
+│ Place       : ᴋᴀɴᴅʏ 🇱🇰
 │ Gender    :  ᴍᴀʟᴇ
 │ Age          : 22_
-│ Phone     : wa.me/923184474176
-│ Youtube   : Youtube.com/c/SuhailTechInfo
-│ Status     : ɢᴀᴍᴇ ᴅᴇᴠᴇʟᴏᴘᴇʀ
+│ Phone     : wa.me/94752799279
+│ Status     : ʙᴏᴛ ᴅᴇᴠᴇʟᴏᴘᴇʀ
 ╰═══ ━ ━ ━ ━ • ━ ━ ━ ━ ═══♡᭄`
 
 
